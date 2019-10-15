cb40e057b04a6858099e553e4aa5739c9c72dd8c
Simon Lara <simon.larah@gmail.com>
2019-10-15 15:45:21 -0300

    caca

--- src/js/layout.js
+++ src/js/layout.js
@@ -4,6 +4,7 @@ import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./views/home";
 import { Demo } from "./views/demo";
+import { Login } from "./views/login";
 import { Single } from "./views/single";
 import injectContext from "./store/appContext";
 
