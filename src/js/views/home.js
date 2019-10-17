52cd0be282e233110e62494f3c871eaeb06069e1
Simon Lara <simon.larah@gmail.com>
2019-10-17 11:18:34 -0300

    inicio dia 3

---
+++ src/js/views/home.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "../../styles/home.scss";
+
+export const Home = () => {
+	return (
+		<div className="container">
+			<form>
+				<div className="form-group">
+					<label>Email address</label>
+					<input
+						type="email"
+						className="form-control"
+						id="exampleInputEmail1"
+						ria-describedby="emailHelp"
+						placeholder="Enter email"
+					/>
+					<small id="emailHelp" className="form-text text-muted">
+						Nunca compartas tu correo y contraseña con otros.
+					</small>
+				</div>
+				<div className="form-group">
+					<label>Password</label>
+					<input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+				</div>
+				<div className="form-group form-check">
+					<input type="checkbox" className="form-check-input" id="exampleCheck1" />
+					<label className="form-check-label">Check me out</label>
+				</div>
+				<button type="submit" className="btn btn-primary">
+					Submit
+				</button>
+			</form>
+		</div>
+	);
+};
