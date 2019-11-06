557ce0cdf532d10d0e64abf17217bae634284e39
Andrés S <sotomayor001@gmail.com>
2019-11-06 14:36:19 +0000

    resolvi problema clients.js

--- src/js/views/clients.js
+++ src/js/views/clients.js
@@ -1,277 +1,262 @@
 import React from "react";
 import "../../styles/home.scss";
 import { Link } from "react-router-dom";
-import { Consumer } from "../store/appContext";
+import layout from "../layout";
 
-import "../../styles/demo.scss";
+export const Clients = () => {
+	return (
+		<div className="container">
+			<div className="table-wrapper">
+				<div className="table-title">
+					<br />
+					<br />
 
-export class Clients extends React.Component {
-	constructor(props) {
-		super(props);
-		this.actionsContext = null;
-		this.state = {
-			inputs: {}
-		};
-	}
-
-	// Guarda datos del input en el state
-	onHandleChange = e => {
-		this.setState({
-			inputs: {
-				...this.state.inputs,
-				[e.target.name]: e.target.value
-			}
-		});
-	};
-
-	onHandleSubmit = () => {
-		console.log("MIS VALORES", this.state.inputs);
-
-		//  actions.saludar("Jorge")
-		this.actionsContext.saveDataCompany(this.state.inputs);
-	};
-
-	render() {
-		return (
-			<>
-				<Consumer>
-					{({ store, actions }) => {
-						this.actionsContext = actions;
-						return (
-							<>
-								<div className="container mb-5">
-									<div className="card">
-										<div className="card-header text-center">
-											<b>Registro de Cliente</b>
-										</div>
-
-										<div className="card-body">
-											<div className="alert alert-danger" role="alert">
-												Faltan algunos campos.
-											</div>
-
-											<div className="form-row">
-												<div className="form-group col-md-4">
-													<label htmlFor="formGroupExampleInput">Empresa</label>
-													<input
-														type="text"
-														className="form-control"
-														id="formGroupExampleInput"
-														name="companyName"
-														placeholder="Razón Social"
-														pattern="[A-Za-z]{25}"
-														onChange={this.onHandleChange}
-													/>
-												</div>
-
-												<div className="form-group col-md-4">
-													<label htmlFor="formGroupExampleInput">Rut</label>
-													<input
-														type="text"
-														className="form-control"
-														id="formGroupExampleInput"
-														placeholder="98.765.432-1"
-														pattern="[0-9]{3}"
-														name="companyRut"
-														onChange={this.onHandleChange}
-													/>
-												</div>
-												<div className="form-group col-md-4">
-													<label htmlFor="formGroupExampleInput">Dirección</label>
-													<input
-														type="text"
-														className="form-control"
-														id="formGroupExampleInput"
-														placeholder="Calle, Comuna"
-														pattern="[A-Za-z]{25}"
-														name="Address"
-														onChange={this.onHandleChange}
-													/>
-												</div>
-											</div>
-
-											<div className="form-row">
-												<div className="form-group col-md-4">
-													<label htmlFor="formGroupExampleInput">Página Web</label>
-													<input
-														type="text"
-														className="form-control"
-														id="formGroupExampleInput"
-														placeholder="http://www.pastelera.cl"
-														name="WebSite"
-														onChange={this.onHandleChange}
-													/>
-												</div>
-
-												<div className="form-group col-md-4">
-													<label htmlFor="formGroupExampleInput">E-mail</label>
-													<input
-														type="text"
-														className="form-control"
-														id="formGroupExampleInput"
-														placeholder="contacto@pastelera.cl"
-														pattern="[A-Za-z]{25}"
-														name="E-mail"
-														onChange={this.onHandleChange}
-													/>
-												</div>
-
-												<div className="form-group col-md-4">
-													<label htmlFor="formGroupExampleInput">Telefono</label>
-													<input
-														type="text"
-														className="form-control"
-														id="formGroupExampleInput"
-														placeholder="+56 9 8765 4321"
-														pattern="[0-9]{11}"
-														name="Telefono"
-														onChange={this.onHandleChange}
-													/>
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-
-								<div className="container">
-									<div className="card">
-										<div className="card-header text-center">
-											<b>Registro de Campaña</b>
-										</div>
-
-										<div className="card-body">
-											<div className="alert alert-danger" role="alert">
-												Faltan algunos campos.
-												<br />
-											</div>
-
-											<div className="form-row">
-												<div className="form-group col-md-4">
-													<label htmlFor="formGroupExampleInput">Fecha de Inicio</label>
-													<input
-														type="text"
-														className="form-control"
-														id="formGroupExampleInput"
-														placeholder="DD/MM/AAAA"
-														pattern="[A-Za-z]{25}"
-														name="Fecha inicio"
-														onChange={this.onHandleChange}
-													/>
-												</div>
-												<div className="form-group col-md-4">
-													<label htmlFor="formGroupExampleInput">Fecha de Termino</label>
-													<input
-														type="text"
-														className="form-control"
-														id="formGroupExampleInput"
-														placeholder="DD/MM/AAAA"
-														pattern="[0-9]{3}"
-														name="Fecha termino"
-														onChange={this.onHandleChange}
-													/>
-												</div>
-												<div className="form-group col-md-4">
-													<label htmlFor="formGroupExampleInput">Comuna</label>
-													<input
-														type="text"
-														className="form-control"
-														id="formGroupExampleInput"
-														placeholder="Desplegable"
-														pattern="[A-Za-z]{25}"
-														name="Comuna"
-														onChange={this.onHandleChange}
-													/>
-												</div>
-											</div>
-
-											<div className="form-row">
-												<div className="form-group col-md-4">
-													<label htmlFor="formGroupExampleInput">
-														Anticipación del Mensaje
-													</label>
-													<input
-														type="text"
-														className="form-control"
-														id="formGroupExampleInput"
-														placeholder="Número de días"
-														name="Dias de anticipacion"
-														onChange={this.onHandleChange}
-													/>
-												</div>
-
-												<div className="form-group col-md-4">
-													<label htmlFor="formGroupExampleInput">Número de SMS</label>
-													<input
-														type="text"
-														className="form-control"
-														id="formGroupExampleInput"
-														placeholder="Limite diario"
-														pattern="[A-Za-z]{25}"
-														name="Numero de SMS"
-														onChange={this.onHandleChange}
-													/>
-												</div>
-
-												<div className="form-group col-md-4">
-													<label htmlFor="formGroupExampleInput">Número de Correos</label>
-													<input
-														type="text"
-														className="form-control"
-														id="formGroupExampleInput"
-														placeholder="Limite diario"
-														pattern="[0-9]{11}"
-														name="Numero de correos"
-														onChange={this.onHandleChange}
-													/>
-												</div>
-												<div className="form-group col-md-6">
-													<label htmlFor="formGroupExampleInput">Mensaje SMS</label>
-													<textarea
-														className="form-control"
-														rows="5"
-														id="comment"
-														onChange={this.onHandleChange}
-														name="ComentarioSMS"
-													/>
-												</div>
-												<div className="form-group col-md-6">
-													<label htmlFor="formGroupExampleInput">Mensaje Email</label>
-													<textarea
-														className="form-control"
-														rows="5"
-														id="comment"
-														onChange={this.onHandleChange}
-														name="ComentarioEmail"
-													/>
-												</div>
-											</div>
-										</div>
-
-										<div className="card-footer text-right">
-											<button type="button" className="btn btn-secondary">
-												Volver
-											</button>
-											<button
-												type="button"
-												className="btn btn-primary ml-1"
-												onClick={() => this.onHandleSubmit()}>
-												Guardar
-											</button>
-										</div>
-									</div>
-
-									<br />
-									<br />
-
-									<Link to="/Clients">
-										<button className="btn btn-primary">Back home</button>
-									</Link>
-								</div>
-							</>
-						);
-					}}
-				</Consumer>
-			</>
-		);
-	}
-}
+					<div className="row">
+						<div className="col-sm-6">
+							<h2>
+								Administrador de
+								<b> Clientes</b>
+							</h2>
+						</div>
+						<div className="col-sm-6">
+							<a href="#addEmployeeModal" className="btn btn-success" data-toggle="modal">
+								<i
+									className="material-icons"
+									data-toggle="tooltip"
+									title=""
+									data-original-title="Edit"
+								/>{" "}
+								<span>Agregar nuevo Cliente</span>
+								<span />
+							</a>
+							<a href="#deleteEmployeeModal" className="btn btn-danger" data-toggle="modal">
+								<i className="material-icons ml-auto"></i> <span>Borrar</span>
+							</a>
+						</div>
+					</div>
+				</div>
+				<table className="table table-striped table-hover">
+					<thead>
+						<tr>
+							<th>
+								<span className="custom-checkbox">
+									<input type="checkbox" id="selectAll" />
+									<label htmlFor="selectAll" />
+								</span>
+							</th>
+							<th>Nombre</th>
+							<th>Email</th>
+							<th>Direccion</th>
+							<th>Telefono</th>
+							<th>Editar/Borrar</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>
+								<span className="custom-checkbox">
+									<input type="checkbox" id="checkbox1" name="options[]" value="1" />
+									<label htmlFor="checkbox1" />
+								</span>
+							</td>
+							<td>Thomas Hardy</td>
+							<td>thomashardy@mail.com</td>
+							<td>89 Chiaroscuro Rd, Portland, USA</td>
+							<td>(171) 555-2222</td>
+							<td>
+								<a href="#editEmployeeModal" className="edit" data-toggle="modal">
+									<i
+										className="material-icons"
+										data-toggle="tooltip"
+										title=""
+										data-original-title="Edit">
+										
+									</i>
+								</a>
+								<a href="#deleteEmployeeModal" className="delete" data-toggle="modal">
+									<i
+										className="material-icons"
+										data-toggle="tooltip"
+										title=""
+										data-original-title="Delete">
+										
+									</i>
+								</a>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<span className="custom-checkbox">
+									<input type="checkbox" id="checkbox2" name="options[]" value="1" />
+									<label htmlFor="checkbox2" />
+								</span>
+							</td>
+							<td>Dominique Perrier</td>
+							<td>dominiqueperrier@mail.com</td>
+							<td>Obere Str. 57, Berlin, Germany</td>
+							<td>(313) 555-5735</td>
+							<td>
+								<a href="#editEmployeeModal" className="edit" data-toggle="modal">
+									<i
+										className="material-icons"
+										data-toggle="tooltip"
+										title=""
+										data-original-title="Edit">
+										
+									</i>
+								</a>
+								<a href="#deleteEmployeeModal" className="delete" data-toggle="modal">
+									<i
+										className="material-icons"
+										data-toggle="tooltip"
+										title=""
+										data-original-title="Delete">
+										
+									</i>
+								</a>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<span className="custom-checkbox">
+									<input type="checkbox" id="checkbox3" name="options[]" value="1" />
+									<label htmlFor="checkbox3" />
+								</span>
+							</td>
+							<td>Maria Anders</td>
+							<td>mariaanders@mail.com</td>
+							<td>25, rue Lauriston, Paris, France</td>
+							<td>(503) 555-9931</td>
+							<td>
+								<a href="#editEmployeeModal" className="edit" data-toggle="modal">
+									<i
+										className="material-icons"
+										data-toggle="tooltip"
+										title=""
+										data-original-title="Edit">
+										
+									</i>
+								</a>
+								<a href="#deleteEmployeeModal" className="delete" data-toggle="modal">
+									<i
+										className="material-icons"
+										data-toggle="tooltip"
+										title=""
+										data-original-title="Delete">
+										
+									</i>
+								</a>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<span className="custom-checkbox">
+									<input type="checkbox" id="checkbox4" name="options[]" value="1" />
+									<label htmlFor="checkbox4" />
+								</span>
+							</td>
+							<td>Fran Wilson</td>
+							<td>franwilson@mail.com</td>
+							<td>C/ Araquil, 67, Madrid, Spain</td>
+							<td>(204) 619-5731</td>
+							<td>
+								<a href="#editEmployeeModal" className="edit" data-toggle="modal">
+									<i
+										className="material-icons"
+										data-toggle="tooltip"
+										title=""
+										data-original-title="Edit">
+										
+									</i>
+								</a>
+								<a href="#deleteEmployeeModal" className="delete" data-toggle="modal">
+									<i
+										className="material-icons"
+										data-toggle="tooltip"
+										title=""
+										data-original-title="Delete">
+										
+									</i>
+								</a>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<span className="custom-checkbox">
+									<input type="checkbox" id="checkbox5" name="options[]" value="1" />
+									<label htmlFor="checkbox5" />
+								</span>
+							</td>
+							<td>Martin Blank</td>
+							<td>martinblank@mail.com</td>
+							<td>Via Monte Bianco 34, Turin, Italy</td>
+							<td>(480) 631-2097</td>
+							<td>
+								<a href="#editEmployeeModal" className="edit" data-toggle="modal">
+									<i
+										className="material-icons"
+										data-toggle="tooltip"
+										title=""
+										data-original-title="Edit">
+										
+									</i>
+								</a>
+								<a href="#deleteEmployeeModal" className="delete" data-toggle="modal">
+									<i
+										className="material-icons"
+										data-toggle="tooltip"
+										title=""
+										data-original-title="Delete">
+										
+									</i>
+								</a>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+				{/* 
+				<div className="clearfix">
+					<div className="hint-text">
+						Showing <b>5</b> out of <b>25</b> entries
+					</div>
+					<ul className="pagination">
+						<li className="page-item disabled">
+							<a href="#">Previous</a>
+						</li>
+						<li className="page-item">
+							<a href="#" className="page-link">
+								1
+							</a>
+						</li>
+						<li className="page-item">
+							<a href="#" className="page-link">
+								2
+							</a>
+						</li>
+						<li className="page-item active">
+							<a href="#" className="page-link">
+								3
+							</a>
+						</li>
+						<li className="page-item">
+							<a href="#" className="page-link">
+								4
+							</a>
+						</li>
+						<li className="page-item">
+							<a href="#" className="page-link">
+								5
+							</a>
+						</li>
+						<li className="page-item">
+							<a href="#" className="page-link">
+								Next
+							</a>
+						</li>
+					</ul>
+				</div>
+				*/}
+			</div>
+		</div>
+	);
+};
